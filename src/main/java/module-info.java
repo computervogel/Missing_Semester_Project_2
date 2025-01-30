9920837993f131861d9d118ee4677d38338dd45b
computervogel <fink.maximilian@gmx.at>
2025-01-30 17:07:16 +0100

    Added AI Picture Generator to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module Project {
     requires javafx.controls;
 
     requires javafx.fxml;
+    requires java.desktop;
+    requires com.google.gson;
+    requires guide;
+    requires okhttp3;
 
     opens main;
 }
